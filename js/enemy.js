a9566f0fc2fe9bd922fa694f564dde6311772059
Jaroslav Nemec <j.nemecc9@gmail.com>
2024-07-02 14:35:18 +0200

    edit movement of enemies

--- js/enemy.js
+++ js/enemy.js
@@ -16,18 +16,18 @@ export default class Enemy extends Phaser.Physics.Arcade.Sprite {
     
   }
 
-  move(playerX, playerY){
+  moveDown(speed){
+    this.setVelocityY(speed)
+  }
+
+  moveToPlayer(playerX, playerY){
     // change angle of enemy to face player, then move towards player
     let playerAngle = Phaser.Math.Angle.Between(this.x, this.y, playerX, playerY);
-
     // todo: fix rotation so enemy ship always faces player
     this.setRotation(playerAngle+3+Math.PI/2);
-    
     // show if enemy is facing player
     // console.log("player angle: ", playerAngle, "enemy angle: ", this.angle-3 + Math.PI*2);
-
     this.scene.physics.moveTo(this, playerX, playerY, this.speed);
-    console.log("moving towards ", playerX, playerY);
   }
 
   takeDamage(damage){
